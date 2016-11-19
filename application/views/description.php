5fd2cf8e79d04c7e1af0b65b8c08f20bcd51444e
DESKTOP-3H8ARI1\anush <anushcs55@gmail.com>
2016-11-19 14:30:46 +0530

    Add Description

--- application/views/description.php
+++ application/views/description.php
@@ -1,1 +1,27 @@
 <!-- Desctiption section -->
+<div class="container" >
+
+
+  <div class="row">
+
+    <div class="col-sm-8 ">
+      <hr class="section-heading-spacer">
+      <div class="clearfix">
+
+      </div>
+      <h2 class="section-heading">
+        Description
+      </h2>
+      <p class="lead">
+        Our aims to establish an all faculty volunteer group network that focuses
+        on student welfare, their social and psychological issues as well as a support
+        group to delegate the work done by the student counselor's office and to needy
+        community outside the university through knowledge of students.
+      </p>
+    </div>
+    <div class="col-sm-4">
+      <img class="img-responsive" src="public/img/holdinghands.png" alt>
+    </div>
+  </div>
+
+</div>
