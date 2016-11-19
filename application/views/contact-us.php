539f00e61e6928c0b67cdfcd3bec33113d81aa9d
Oshan Mudannayake <oshan.ivantha@gmail.com>
2016-11-19 16:18:58 +0530

    Contact info

--- application/views/contact-us.php
+++ application/views/contact-us.php
@@ -1,10 +1,29 @@
 <div class="container">
     <div class="row">
-        <div class="col-lg-2">
-            The first column
+        <div class="col-lg-12">
+            <p class="text-center">
+                Contact information
+            </p>
         </div>
-        <div class="col-lg-2">
-            The second column 
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="row">
+                Hotline :
+            </div>
+            <br>
+            <div class="row">
+                Email :
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="row">
+                Tel :
+            </div>
+            <br>
+            <div class="row">
+                Address :
+            </div>
         </div>
     </div>
 </div>
