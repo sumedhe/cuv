1c89d3bff92849ad540540edd39dbc89ee40f604
sumedhe <sumedhe5dms@gmail.com>
2016-11-19 15:55:58 +0530

    Update paths of links

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,6 @@
-    <script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
-    <script src="<?php echo base_url('assets/js/lodash.min.js') ?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
-    <script src="<?php echo base_url('assets/js/script.js') ?>"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/lodash.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/script.js"></script>
   </body>
 </html>
