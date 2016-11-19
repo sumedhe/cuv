fde9d48bc7518a43a8c9003e6cc2a64b4c35d734
DESKTOP-3H8ARI1\anush <anushcs55@gmail.com>
2016-11-19 16:16:54 +0530

    update vision and mission

--- application/views/mission-vision.php
+++ application/views/mission-vision.php
@@ -1,7 +1,7 @@
 <!-- Mission and Vision section -->
 <div class="container">
-  <div class="col-md-6 wow fadeInRight animated" style=
-  "visibility: visible; animation-name: fadeInRight;">
+  <div class="col-md-6 wow fadeInLeft animated" style=
+  "visibility: visible; animation-name: fadeInLeft;">
   <img alt src="public/img/Table-hands.png" class="so-widget-image"
   style="max-width:100%; height:auto; display:block">
  </div>
