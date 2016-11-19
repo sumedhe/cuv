326bcbb45518e6ca5f7ff31f18b6f8783303c6c8
Oshan Mudannayake <oshan.ivantha@gmail.com>
2016-11-19 16:39:36 +0530

    Contact info partially completed

--- application/views/contact-us.php
+++ application/views/contact-us.php
@@ -1,28 +1,58 @@
 <div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-            <p class="text-center">
-                Contact information
-            </p>
-        </div>
-    </div>
+<!--    <div class="row">-->
+<!--        <div class="col-lg-12">-->
+            <div id="committee" class="container-fluid text-center">
+                <h2>Contact information</h2>
+            </div>
+<!--        </div>-->
+<!--    </div>-->
     <div class="row">
         <div class="col-lg-6">
             <div class="row">
-                Hotline :
+                <div class="col-lg-2">
+                    Hotline :
+                </div>
+                <div class="col-lg-3">
+                    +94 7x xxxxxxx
+                </div>
             </div>
             <br>
             <div class="row">
-                Email :
+                <div class="col-lg-2">
+                    Email :
+                </div>
+                <div class="col-lg-3">
+                    xxxx.cuv@gmail.com
+                </div>
             </div>
         </div>
         <div class="col-lg-6">
             <div class="row">
-                Tel :
+                <div class="col-lg-2">
+                    Tel :
+                </div>
+                <div class="col-lg-3">
+                    <div class="row">
+                        +94 7x xxxxxxx
+                    </div>
+                    <div class="row">
+                        +94 7x xxxxxxx
+                    </div>
+                </div>
             </div>
             <br>
             <div class="row">
-                Address :
+                <div class="col-lg-2">
+                    Address :
+                </div>
+                <div class="col-lg-6">
+                    <address>
+                        <strong>University of Colombo</strong><br>
+                        35 Philip Gunawardena Mawatha<br>
+                        Colombo<br>
+                        00400
+                    </address>
+                </div>
             </div>
         </div>
     </div>
