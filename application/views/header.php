cc6d663c2b9854042c66ace07074946c15e04f68
sumedhe <sumedhe5dms@gmail.com>
2016-11-19 12:59:33 +0530

    Add view for Navbar and Description

--- application/views/header.php
+++ application/views/header.php
@@ -5,12 +5,11 @@
     <meta http-equiv="Content-Type" context="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-    <meta name="description" content="">
-
     <title>Colombo University Volunteers | CUV</title>
 
     <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/font-awesome.min.css') ?>" rel="stylesheet">
-    <link href=<?php echo base_url('assets/css/style.css') ?> rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/style.css') ?>" rel="stylesheet">
+
   </head>
-  <body>
+<body id="home" data-spy="scroll" data-target=".navbar" data-offset="60">
