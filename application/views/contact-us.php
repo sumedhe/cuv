f4dc8da5bd6cb72c239b100cab357c0390fac4cd
apramodya <pramodyaabeysinghe@gmail.com>
2016-11-22 23:33:52 +0530

    contact us-updated

--- application/views/contact-us.php
+++ application/views/contact-us.php
@@ -1,34 +1,45 @@
 <br><br>
-<div id="mission-vision" class="container-fluid bg-grey text-center">
-    <div class="container">
-        <div id="committee" class="container-fluid text-center">
-            <h2>Contact information</h2>
+<div id="contact-us" class="container-fluid bg-grey ">
+        <h2 class="text-center">Contact Us</h2>
+        <br>
+        <div class="row slideanim">
+            <div class="col-lg-5">
+                <br><br><br>
+
+                <table class="table table-condensed table-hover ">
+                    <tbody>
+                        <tr>
+                            <td><span class="met_color glyphicon glyphicon-earphone"></span></td>
+                            <td style="font-weight: bold">+94 xxx xxxxxxx</td>
+                        </tr>
+                        <tr>
+                            <td><span class="met_color glyphicon glyphicon-envelope"></span></td>
+                            <td style="font-weight: bold">admin.cuv@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <td><span class="met_color glyphicon glyphicon-phone"></span></td>
+                            <td style="font-weight: bold">+94 711 303 303</td>
+                        </tr>
+                        <tr>
+                            <td><span class="met_color glyphicon glyphicon-map-marker"></span></td>
+                            <td style="font-weight: bold">University of Colombo, Colombo 7</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-lg-7">
+                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Funiversity.colombo%2F&tabs=timeline&width=1000&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=562525020600615" width="1200" height="300" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+            </div>
         </div>
+        <br><br>
         <div class="row">
-            <div class="col-lg-6">
-                <div class="row">
-                    <span class="met_color glyphicon glyphicon-tag"></span>
-                    HotLine : +94 xxx xxxxxxx
-                </div>
-                <br>
-                <div class="row">
-                    <span class="met_color glyphicon glyphicon-envelope"></span>
-                    E-Mail : xxxx.cuv@gmail.com
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="row">
-                    <span class="met_color glyphicon glyphicon-earphone"> Tel</span>
-                    <div>: +94 912 248 460</div>
-                    <div>: +94 711 303 303</div>
-                </div>
-                <br>
-                <div class="row">
-                    <span class="met_color glyphicon glyphicon-map-marker"> </span>
-                    Address : University of Colombo. 35 Philip Gunawardena Mawatha, Colombo 00400
-                </div>
+            <div class="col-lg-12">
+            <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.902604457124!2d79.85912631419224!3d6.902249995012642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25963120b1509%3A0x2db2c18a68712863!2sUniversity+of+Colombo+School+of+Computing+(UCSC)!5e0!3m2!1sen!2slk!4v1479538480778"
+            width="1000" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
         </div>
+</div>
     </div>
 </div>
 <br>
