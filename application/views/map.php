de8f30c5875945669c3b509cdb4075fb4a8f88c0
sumedhe <sumedhe5dms@gmail.com>
2016-11-23 01:58:02 +0530

    Make map autoresize

--- application/views/map.php
+++ application/views/map.php
@@ -2,6 +2,6 @@
     <div class="row">
         <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.902604457124!2d79.85912631419224!3d6.902249995012642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25963120b1509%3A0x2db2c18a68712863!2sUniversity+of+Colombo+School+of+Computing+(UCSC)!5e0!3m2!1sen!2slk!4v1479538480778"
-            width="1200" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+            width=100% height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
 </div>
