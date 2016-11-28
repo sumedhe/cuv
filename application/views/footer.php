511dc295ec37f3c39fb581835d21c844ae65b3e6
sumedhe <sumedhe5dms@gmail.com>
2016-11-29 02:08:45 +0530

    Update footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,6 @@
+        <div class="container-fluid text-center">
+                Colombo University Volunteers · University of Colombo · 2016
+        </div>
     </div>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/lodash.min.js"></script>
