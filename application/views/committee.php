c9d60765b42b8623e7c094f92bc1aa820210d1bf
sumedhe <sumedhe5dms@gmail.com>
2016-11-28 08:07:59 +0530

    Add committee photos

--- application/views/committee.php
+++ application/views/committee.php
@@ -1,61 +1,136 @@
 <!-- Committee section -->
 <!-- Name, Photo, Designation and Email address of each committee member -->
 <!-- Container (Portfolio Section) -->
-  <div id="committee" class="container-fluid text-center">
-  <h2>COMMITTEE</h2>
-  <h4>Official Committee for 2015/16</h4>
-  <br>
-  <div class="row slideanim">
-  	<div class="col-sm-3"></div>
-    <div class="col-sm-3">
-      <img src="public/img/anon.jpg" height="50%" width="50%">
-      <h4>position</h4>
-      <p class="names">name</p>
-      <p>email</p>
+<div id="committee" class="row container-fluid text-center">
+    <h2>COMMITTEE</h2>
+    <h4>Official Committee</h4>
+    <br>
+
+    <div class="col-sm-1"></div>
+    <div class="col-sm-10">
+
+        <!-- ROW 1 -->
+        <div class="row slideanim">
+            <div class="col-sm-1"></div>
+
+            <div class="col-sm-10">
+                <div class="col-sm-4">
+                    <img src="public/img/committee/Dr.M.T.M.Mahees.jpg" height="100" width="100">
+                    <h4>Dr.M.T.M.Mahees</h4>
+                    <!-- <p class="names">name</p> -->
+                    <!-- <p>email</p> -->
+                </div>
+
+                <div class="col-sm-4">
+                    <img src="public/img/committee/Ms.Nirosha_Kulasekara.jpg" height="100" width="100">
+                    <h4>Ms.Nirosha Kulasekara</h4>
+                    <!-- <p class="names">name</p> -->
+                    <!-- <p>email</p> -->
+                </div>
+
+                <div class="col-sm-4">
+                    <img src="public/img/committee/Ms.Duleeka_Jayawardhana.jpg" height="100" width="100">
+                    <h4>Ms.Duleeka Jayawardhana</h4>
+                    <!-- <p class="names">name</p> -->
+                    <!-- <p>email</p> -->
+                </div>
+
+            </div>
+
+            <div class="col-sm-1"></div>
+        </div>
+
+        <!-- ROW 2 -->
+        <br><br>
+        <div class="row slideanim">
+            <div class="col-sm-3">
+                <img src="public/img/committee/D.M.Sampath_Darshana_Lakmal.jpg" height="100" width="100">
+                <h4>D.M.Sampath Darshana Lakmal</h4>
+                <p class="names">Faculty of Law</p>
+                <!-- <p>email</p> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="public/img/committee/Kulan_Sachinthana.jpg" height="100" width="100">
+                <h4>Kulan Sachinthana</h4>
+                <p class="names">University of Colombo School of Computing</p>
+                <!-- <p>email</p> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="public/img/committee/K.G.K.Anuradha.jpg" height="100" width="100">
+                <h4>K.G.K.Anuradha</h4>
+                <p class="names">Faculty of Science</p>
+                <!-- <p>email</p> -->
+            </div>
+
+            <div class="col-sm-3">
+              <img src="public/img/committee/Naduni_Jayasinghe.jpg" height="100" width="100">
+              <h4>Naduni Jayasinghe</h4>
+              <p class="names">Faculty of Arts</p>
+              <!-- <p>email</p> -->
+            </div>
+
+
+
+        </div>
+
+        <!-- ROW 3 -->
+        <br><br>
+        <div class="row slideanim">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+
+                <div class="col-sm-4">
+                    <img src="public/img/committee/Poornima_Thathsarani.jpg" height="100" width="100">
+                    <h4>Poornima Thathsarani</h4>
+                    <p class="names">Faculty of Management and Finance</p>
+                    <!-- <p>email</p> -->
+                </div>
+
+                <div class="col-sm-4">
+                    <img src="public/img/committee/P.V.Imalka_Madhuwanthi.jpg" height="100" width="100">
+                    <h4>P.V.Imalka Madhuwanthi</h4>
+                    <p class="names">Faculty of Education</p>
+                    <!-- <p>email</p> -->
+                </div>
+
+                <div class="col-sm-4">
+                  <img src="public/img/committee/Rev.Bro.Dom_Yohan_Wickramasekara.jpg" height="100" width="100">
+                  <h4>Rev.Bro.Dom Yohan Wickramasekara</h4>
+                  <p class="names">Faculty of Arts</p>
+                  <!-- <p>email</p> -->
+                </div>
+
+
+            </div>
+            <div class="col-sm-1"></div>
+        </div>
+
+        <!-- ROW 4 -->
+        <br><br>
+        <div class="row slide-anim">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-3">
+                <img src="public/img/committee/M.D.Sujan_Nayanajith.jpg" height="100" width="100">
+                <h4>M.D.Sujan Nayanajith</h4>
+                <p class="names">Institute of Indigenous Medicine</p>
+                <!-- <p>email</p> -->
+            </div>
+
+            <div class="col-sm-3">
+                <img src="public/img/committee/Kasun_Kumarage.jpg" height="100" width="100">
+                <h4>Kasun Kumarage</h4>
+                <p class="names">Sri Palee Campus</p>
+                <!-- <p>email</p> -->
+            </div>
+
+            <div class="col-sm-3"></div>
+        </div>
+
 
     </div>
-    <div class="col-sm-3">
-      <img src="public/img/anon.jpg" height="50%" width="50%">
-      <h4>position</h4>
-      <p class="names">name</p>
-      <p>email</p>
-
-    </div>
-    <div class="col-sm-3"></div>
-
-  </div>
-  <br><br>
-  <div class="row slideanim">
-    <div class="col-sm-3">
-      <img src="public/img/anon.jpg" height="50%" width="50%">
-      <h4>position</h4>
-      <p class="names">name</p>
-      <p>email</p>
-
-    </div>
-    <div class="col-sm-3">
-      <img src="public/img/anon.jpg" height="50%" width="50%">
-      <h4>position</h4>
-      <p class="names">name</p>
-      <p>email</p>
-
-    </div>
-    <div class="col-sm-3">
-<img src="public/img/anon.jpg" height="50%" width="50%">
-      <h4>position</h4>
-      <p class="names">name</p>
-      <p>email</p>
-
-    </div>
-    <div class="col-sm-3">
-      <img src="public/img/anon.jpg" height="50%" width="50%">
-      <h4>position</h4>
-      <p class="names">name</p>
-      <p>email</p>
-      <!--p><span class="glyphicon glyphicon-phone"></span>Phone: 071 1212121</p-->
-    </div>
-  </div>
-  <br><br>
-
+    <div class="col-sm-1"></div>
+    <br><br>
 </div>
 <br><br>
