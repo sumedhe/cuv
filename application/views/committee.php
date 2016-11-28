814643983d17796907b0da44cbd3d0ce6ea82252
sumedhe <sumedhe5dms@gmail.com>
2016-11-29 02:10:23 +0530

    Update committee

--- application/views/committee.php
+++ application/views/committee.php
@@ -11,67 +11,67 @@
 
         <!-- ROW 1 -->
         <div class="row slideanim">
-            <div class="col-sm-1"></div>
 
-            <div class="col-sm-10">
-                <div class="col-sm-4">
-                    <img src="public/img/committee/Dr.M.T.M.Mahees.jpg" height="100" width="100">
-                    <h4>Dr.M.T.M.Mahees</h4>
-                    <!-- <p class="names">name</p> -->
-                    <!-- <p>email</p> -->
-                </div>
-
-                <div class="col-sm-4">
-                    <img src="public/img/committee/Ms.Nirosha_Kulasekara.jpg" height="100" width="100">
-                    <h4>Ms.Nirosha Kulasekara</h4>
-                    <!-- <p class="names">name</p> -->
-                    <!-- <p>email</p> -->
-                </div>
-
-                <div class="col-sm-4">
-                    <img src="public/img/committee/Ms.Duleeka_Jayawardhana.jpg" height="100" width="100">
-                    <h4>Ms.Duleeka Jayawardhana</h4>
-                    <!-- <p class="names">name</p> -->
-                    <!-- <p>email</p> -->
-                </div>
-
+            <div class="col-sm-3">
+                <img src="public/img/committee/Dr.M.T.M.Mahees.jpg" height="100" width="100">
+                <h4>Dr.M.T.M.Mahees</h4>
+                <!-- <p class="names">name</p> -->
+                <!-- <p>email</p> -->
             </div>
 
-            <div class="col-sm-1"></div>
-        </div>
+            <div class="col-sm-3">
+                <img src="public/img/committee/Ms.Nirosha_Kulasekara.jpg" height="100" width="100">
+                <h4>Ms.Nirosha Kulasekara</h4>
+                <!-- <p class="names">name</p> -->
+                <!-- <p>email</p> -->
+            </div>
 
-        <!-- ROW 2 -->
-        <br><br>
-        <div class="row slideanim">
+            <div class="col-sm-3">
+                <img src="public/img/committee/Ms.Duleeka_Jayawardhana.jpg" height="100" width="100">
+                <h4>Ms.Duleeka Jayawardhana</h4>
+                <!-- <p class="names">name</p> -->
+                <!-- <p>email</p> -->
+            </div>
+
             <div class="col-sm-3">
                 <img src="public/img/committee/D.M.Sampath_Darshana_Lakmal.jpg" height="100" width="100">
-                <h4>D.M.Sampath Darshana Lakmal</h4>
+                <h4>D.M.Sampath Darshana Lakshan</h4>
                 <p class="names">Faculty of Law</p>
                 <!-- <p>email</p> -->
             </div>
 
-            <div class="col-sm-3">
-                <img src="public/img/committee/Kulan_Sachinthana.jpg" height="100" width="100">
-                <h4>Kulan Sachinthana</h4>
-                <p class="names">University of Colombo School of Computing</p>
-                <!-- <p>email</p> -->
+        </div>
+
+        <h4>Faculty Leaders</h4>
+        <!-- ROW 2 -->
+        <br><br>
+        <div class="row slideanim">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+
+                <div class="col-sm-4">
+                    <img src="public/img/committee/Kulan_Sachinthana.jpg" height="100" width="100">
+                    <h4>Kulan Sachinthana</h4>
+                    <p class="names">University of Colombo School of Computing</p>
+                    <!-- <p>email</p> -->
+                </div>
+
+                <div class="col-sm-4">
+                    <img src="public/img/committee/K.G.K.Anuradha.jpg" height="100" width="100">
+                    <h4>K.G.K.Anuradha</h4>
+                    <p class="names">Faculty of Science</p>
+                    <!-- <p>email</p> -->
+                </div>
+
+                <div class="col-sm-4">
+                    <img src="public/img/committee/Poornima_Thathsarani.jpg" height="100" width="100">
+                    <h4>Poornima Thathsarani</h4>
+                    <p class="names">Faculty of Management and Finance</p>
+                    <!-- <p>email</p> -->
+                </div>
             </div>
 
-            <div class="col-sm-3">
-                <img src="public/img/committee/K.G.K.Anuradha.jpg" height="100" width="100">
-                <h4>K.G.K.Anuradha</h4>
-                <p class="names">Faculty of Science</p>
-                <!-- <p>email</p> -->
-            </div>
-
-            <div class="col-sm-3">
-              <img src="public/img/committee/Naduni_Jayasinghe.jpg" height="100" width="100">
-              <h4>Naduni Jayasinghe</h4>
-              <p class="names">Faculty of Arts</p>
-              <!-- <p>email</p> -->
-            </div>
-
-
+            <div class="col-sm-1"></div>
 
         </div>
 
@@ -82,16 +82,9 @@
             <div class="col-sm-10">
 
                 <div class="col-sm-4">
-                    <img src="public/img/committee/Poornima_Thathsarani.jpg" height="100" width="100">
-                    <h4>Poornima Thathsarani</h4>
-                    <p class="names">Faculty of Management and Finance</p>
-                    <!-- <p>email</p> -->
-                </div>
-
-                <div class="col-sm-4">
-                    <img src="public/img/committee/P.V.Imalka_Madhuwanthi.jpg" height="100" width="100">
-                    <h4>P.V.Imalka Madhuwanthi</h4>
-                    <p class="names">Faculty of Education</p>
+                    <img src="public/img/committee/Naduni_Jayasinghe.jpg" height="100" width="100">
+                    <h4>Naduni Jayasinghe</h4>
+                    <p class="names">Faculty of Arts</p>
                     <!-- <p>email</p> -->
                 </div>
 
@@ -102,6 +95,13 @@
                   <!-- <p>email</p> -->
                 </div>
 
+                <div class="col-sm-4">
+                    <img src="public/img/committee/P.V.Imalka_Madhuwanthi.jpg" height="100" width="100">
+                    <h4>P.V.Imalka Madhuwanthi</h4>
+                    <p class="names">Faculty of Education</p>
+                    <!-- <p>email</p> -->
+                </div>
+
 
             </div>
             <div class="col-sm-1"></div>
@@ -110,22 +110,31 @@
         <!-- ROW 4 -->
         <br><br>
         <div class="row slide-anim">
-            <div class="col-sm-3"></div>
-            <div class="col-sm-3">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+            <div class="col-sm-4">
                 <img src="public/img/committee/M.D.Sujan_Nayanajith.jpg" height="100" width="100">
                 <h4>M.D.Sujan Nayanajith</h4>
                 <p class="names">Institute of Indigenous Medicine</p>
                 <!-- <p>email</p> -->
             </div>
 
-            <div class="col-sm-3">
+            <div class="col-sm-4">
                 <img src="public/img/committee/Kasun_Kumarage.jpg" height="100" width="100">
                 <h4>Kasun Kumarage</h4>
                 <p class="names">Sri Palee Campus</p>
                 <!-- <p>email</p> -->
             </div>
 
-            <div class="col-sm-3"></div>
+            <div class="col-sm-4">
+                <img src="public/img/committee/K.G.Dilshan_Chameera_Karunarathna.jpg" height="100" width="100">
+                <h4>K.G.Dilshan Chameera Karunarathna</h4>
+                <p class="names">Faculty of Medicine</p>
+                <!-- <p>email</p> -->
+            </div>
+
+            </div>
+            <div class="col-sm-1"></div>
         </div>
 
 
